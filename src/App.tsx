814dc9311172a9c387f3f566f21d6c873afb326f
Eduardo Russo <russoedu@gmail.com>
2023-12-07 07:42:31 +0000

    Simplified theme, updated clean button display

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from '@emotion/react'
 import { Container, createTheme, CssBaseline, StyledEngineProvider } from '@mui/material'
 import fontColorContrast from 'font-color-contrast'
-import { StrictMode, useState } from 'react'
+import { StrictMode } from 'react'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Calc } from './_components/Calc'
 import { Footer } from './_components/Footer'
@@ -10,31 +10,29 @@ import './App.css'
 import { appName, ThemeT } from './helpers/types'
 
 export function App () {
-  const [colour] = useState('#fff')
-  const [tm] = useState({
+  const tm: ThemeT = {
     mode:         'light',
     highContrast: false,
     colour:       '#894fd6',
     width:        10,
     height:       10,
-  } as ThemeT)
+  }
 
-  const theme = createTheme({
-    palette: {
-      getContrastText: fontColorContrast,
-      mode:            tm.mode,
-      background:      {
-        default: tm.mode === 'dark' ? '#000' : '#fff',
-        paper:   tm.mode === 'dark' ? '#000' : '#fff',
-      },
-      primary: {
-        main: tm.colour,
+  const theme = createTheme(
+    {
+      palette: {
+        getContrastText: fontColorContrast,
+        mode:            tm.mode,
+        background:      {
+          default: '#fff',
+          paper:   '#fff',
+        },
+        primary: {
+          main: tm.colour,
+        },
       },
     },
-  },
-  [
-    { iconColour: colour },
-  ])
+  )
   
   return (
     <StrictMode>
