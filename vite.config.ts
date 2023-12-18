17d01ce090515bdcff4dff0e9aa222b727bb3d93
Eduardo Russo <russoedu@gmail.com>
2023-12-18 21:38:04 +0000

    updated name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins:      [react()],
-  base:         '/numerologia-react/',
+  base:         '/num/',
   optimizeDeps: {
     include: [
       '@emotion/react', 
